eff56ba422f7e47a1b629696ebd84e253bf7abe1
Jose Tapia <jose.tapia@ki.se>
2017-11-14 15:57:54 +0100

    Express Simple REST

--- index.js
+++ index.js
@@ -16,12 +16,13 @@ app.all('/dishes', (req, res, next) => {
     next();
 });
 
-app.get('/dishes', (res, req, next) => {
+app.get('/dishes', (req, res, next) => {
     res.end('Will send all the dishes to you!');
 });
 
 app.post('/dishes', (req, res, next) => {
-    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+    res.end('Will add the dish: ' + req.body.name + 
+        ' with details: ' + req.body.description);
 });
 
 app.put('/dishes', (req, res, next) => {
@@ -29,25 +30,29 @@ app.put('/dishes', (req, res, next) => {
     res.end('PUT operation not supported on /dishes');
 });
 
-app.delete('/dishes', (res, req, next) => {
-    res.end('Deleting all the dishes!');
+app.delete('/dishes', (req, res, next) => {
+    res.end('Deleting all dishes');
 });
 
-app.get('/dishes/:dishId', (res, req, next) => {
-    res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
+app.get('/dishes/:dishId', (req, res, next) => {
+    res.end('Will send details of the dish: ' 
+        + req.params.dishId + ' to you!');
 });
 
 app.post('/dishes/:dishId', (req, res, next) => {
-    res.end('POST operation not supported on /dishes/:' + req.params.dishId);
+    res.statusCode = 403;
+    res.end('POST operation not supported on /dishes/' 
+        + req.params.dishId);
 });
 
 app.put('/dishes/:dishId', (req, res, next) => {
     res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
+    res.end('Will update the dish: ' + req.body.name + 
+        ' with details: ' + req.body.description);
 });
 
-app.delete('/dishes/:dishId', (res, req, next) => {
-    res.end('Deleting the dish: ' + req.params.dishId);
+app.delete('/dishes/:dishId', (req, res, next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
 });
 
 app.use(express.static(__dirname + '/public'));
